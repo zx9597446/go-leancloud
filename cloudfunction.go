3ebd60ab4aaaf6a49f8ff3d84818928ff0e542c8
zx9597446 <zx9597446@gmail.com>
2014-12-01 15:22:39 +0800

    new utility function for cloud

--- cloudfunction.go
+++ cloudfunction.go
@@ -4,3 +4,11 @@ func (cloud *Cloud) CloudFunction(fn string, jsonParam string) (*Result, error) 
 	url := cloud.makeURLPrefix("functions", fn)
 	return cloud.HttpPost(url, jsonParam)
 }
+
+func (cloud *Cloud) CloudFunctionResultAsObject(fn, jsonParam, className string) (*Object, error) {
+	r, err := cloud.CloudFunction(fn, jsonParam)
+	if err != nil {
+		return nil, err
+	}
+	return r.Decode(className)
+}
